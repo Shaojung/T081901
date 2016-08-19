cd8bda77f4755421a7790b3dc38484cdb36d02a6
shaojung <shaojung@gmail.com>
2016-08-19 11:29:52 +0800

    GCM Start!!

--- app/src/main/java/tw/com/pcschool/t081901/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t081901/MainActivity.java
@@ -6,18 +6,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SENDER_ID = "518538475960";
+    private GoogleCloudMessaging gcm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                gcm = GoogleCloudMessaging.getInstance(MainActivity.this);
+                try {
+                    String regid = gcm.register(SENDER_ID);
+                    Log.d("GCM", regid);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+    }
     public void click1(View v)
     {
         Calendar cal = Calendar.getInstance();
